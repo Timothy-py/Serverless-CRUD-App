6fa13faea8ddcb88267ca6e0975de2bac6ecf75e
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-09-25 13:47:22 +0100

    Enable update and delete of product by unique id instead of name, and do some little documentation

--- Product_App/app.py
+++ Product_App/app.py
@@ -11,7 +11,7 @@ es = Elasticsearch()
 
 import json
 
-from bson import ObjectId       # necessary for getting the _id field of each data product from mongoDB
+from bson.objectid import ObjectId       # necessary for getting the _id field of each data product from mongoDB
 
 app = Chalice(app_name='Product_App')
 
@@ -22,40 +22,52 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+"""
+A simple CRUD serverless application for Products
+"""
 
+# Index function
 @app.route('/')
 def index():
     return {'hello': 'Welcome to Product Shop'}
 
-
+# Create a product function
 @app.route('/create', methods=['POST'])
 def create():
-    request = app.current_request
-    body = request.json_body
-    records.insert_one(body)
+    request = app.current_request                               # this attribute is used to introspect the current HTTP request and save inside the request var
+    product = request.json_body                                 # get the json object passed by client and save it inside the product var
+    records.insert_one(product)                                 # mongoDB syntax for inseting a document i.e product, into the collection i.e DB table
     return {'feedback': 'Product Created Successfully'}
 
+# Read All products function
 @app.route('/read', methods=['GET'])
 def readAll():
     return JSONEncoder().encode(list(records.find()))
 
-@app.route('/{name}')
-def readOne(name):
-    product = records.find_one({'name': name})
+# Read one product function
+@app.route('/{product_id}')
+def readOne(product_id):
+    key = dict(_id=ObjectId(product_id))                        # the ObjectId function creates a new unique identifier with the product_id suppplied and then
+                                                                # a key-value pair is created with it, with _id as key, and then save it inside the key var
+    product = records.find_one(key)                             # mongoDB syntax for finding a document
     return JSONEncoder().encode(product)
 
-@app.route('/{name}/update', methods=['POST'])
-def update(name):
+# Update a product function
+@app.route('/{product_id}/update', methods=['PUT'])
+def update(product_id):
     request = app.current_request
-    product = records.find_one({'name': name})
-    body = request.json_body
-    new_product = {'$set': body}
-    records.update_one(data, new_data)
+    key = dict(_id=ObjectId(product_id))
+    old_product = records.find_one(key)
+    new_product = request.json_body
+    new_product = {'$set': new_product}
+    records.update_one(old_product, new_product)
     return {'feedback': 'Product Updated Successfully'}
 
-@app.route('/{name}/delete', methods=['POST'])
-def delete(name):
-    product = records.find_one({'name': name})
+# Delete a product function
+@app.route('/{product_id}/delete', methods=['DELETE'])
+def delete(product_id):
+    key = dict(_id=ObjectId(product_id))
+    product = records.find_one(key)
     if product is not None:
         records.delete_one(product)
         return {'feedback': 'Product Deleted Successfully'}
